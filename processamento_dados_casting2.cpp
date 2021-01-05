764a69570d872513b7de963686902ad0c0b02322
Gabriel Filipe Da Costa <76450972+Ssxund3r@users.noreply.github.com>
2021-01-05 16:25:48 -0300

    Lógica_ programação_3.C++

---
+++ processamento_dados_casting2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main ()
+{
+  int x, y;
+  x = 5;
+  y = 2 * x;
+
+  cout << x << endl;
+  cout << y << endl;
+
+  return 0;
+}
+
---
+++ processamento_dados_casting5.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main ()
+{
+  int a, b, resultado;
+
+  a = 5;
+  b = 2;
+  resultado = a / b;
+
+  cout << resultado << endl;
+
+ return 0;
+}
