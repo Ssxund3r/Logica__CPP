7d9101385562af4df36b56f9bf3eafcb3a866644
Gabriel Filipe Da Costa <76450972+Ssxund3r@users.noreply.github.com>
2021-01-06 10:17:55 -0300

    Lógica_ programação_3.C++

---
+++ estrutura_condicional.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main ()
+{
+   if (condicao) {   /* comando simples */
+    comando 1
+    comando 2
+   }
+
+
+   if (condicao) {
+    comando 1
+    comando 2
+   }                    /* comando composto */
+   else {
+    comando 3
+    comando 4
+   }
+
+
+   if (condicao1) {
+      comando1
+      comando2
+   }
+   else if(condicao2){
+      comando3
+      comando4
+   }                        /* comando encadeamento */
+   else {
+    comando5
+    comando6
+   }
+
+ return 0;
+}
---
+++ estrutura_condicional2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main ()
+{
+    int hora;
+    cout << "Digite uma hora do dia: ";
+    cin >> hora;
+    if (hora < 12){
+        cout << "Bom dia!" << endl;
+    }
+    else {
+        cout << "Boa tarde!" << endl;
+    }
+
+   return 0;
+}
